88788c7c0b20a4252cf368d9a0ba3e3a97b078de
RickWoltheus <rwoltheus@gmail.com>
2019-04-11 09:56:20 +0200

    add typing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { LocalizationProvider, initializeLocalization } from './services/LocalizationService'
 import { Root } from './screens/Root'
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createStackNavigator, createAppContainer, NavigationScreenProp, NavigationRouter } from 'react-navigation'
 import { KitchenSinkTabView } from './BP/KitchenSink/KitchenSink'
-import { ThemeProvider } from './providers/ThemeProvider';
 
 const RootNavigator = createStackNavigator({
     Root,
@@ -19,17 +18,15 @@ const RootNavigator = createStackNavigator({
         mode: 'modal',
     })
 
-export class AppProviders extends React.Component<{ navigation: any }> {
+export class AppProviders extends React.Component<{ navigation: NavigationScreenProp<{}> }> {
 
-    public static router: any = RootNavigator.router
+    public static router: NavigationRouter<{}, {}> = RootNavigator.router
 
     public render() {
         return (
             <View style={styles.container}>
                 <LocalizationProvider initialize={initializeLocalization}>
-                    <ThemeProvider>
-                        <RootNavigator navigation={this.props.navigation} />
-                    </ThemeProvider>
+                    <RootNavigator navigation={this.props.navigation} />
                 </LocalizationProvider>
             </View>
         )
