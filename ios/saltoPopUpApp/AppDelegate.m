24f26a97dfc240db6ef8d9009029e0ebd90b6304
Rick Woltheus <rwoltheus@gmail.com>
2019-05-27 13:18:06 +0200

    feat(): add firebase pods

--- ios/saltoPopUpApp/AppDelegate.m
+++ ios/saltoPopUpApp/AppDelegate.m
@@ -6,6 +6,7 @@
  */
 
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -15,6 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"artifacts/src/index" fallbackResource:nil];
