5a30581a870f05a1dfe3cf4fe9ad50c06d977042
Rick Woltheus <rwoltheus@gmail.com>
2019-06-17 11:36:31 +0200

    feat(browser): add debounce

--- src/services/Browser.tsx
+++ src/services/Browser.tsx
@@ -1,30 +1,35 @@
 import { Platform, Linking } from "react-native";
 import SafariView from "react-native-safari-view";
 import { CustomTabs } from "react-native-custom-tabs";
+import { debounce } from "lodash";
 
-export function openPlatformSpecificWebViews(url: string) {
-    if (Platform.OS === 'ios') {
-        SafariView.show({
-            url: url,
-            fromBottom: true,
-        })
-        return
-    }
+export const openPlatformSpecificWebViews = debounce(
+    (url: string) => {
+        if (Platform.OS === 'ios') {
+            SafariView.show({
+                url: url,
+                fromBottom: true,
+            })
+            return
+        }
 
-    if (Platform.OS === 'android') {
-        CustomTabs.openURL(url, {
-            enableUrlBarHiding: true,
-            showPageTitle: true,
-            enableDefaultShare: true,
-            animations: {
-                startEnter: 'slide_in_bottom',
-                startExit: 'slide_out_bottom',
-                endEnter: 'slide_in_bottom',
-                endExit: 'slide_out_bottom',
-            },
-        })
-        return
-    }
+        if (Platform.OS === 'android') {
+            CustomTabs.openURL(url, {
+                enableUrlBarHiding: true,
+                showPageTitle: true,
+                enableDefaultShare: true,
+                animations: {
+                    startEnter: 'slide_in_bottom',
+                    startExit: 'slide_out_bottom',
+                    endEnter: 'slide_in_bottom',
+                    endExit: 'slide_out_bottom',
+                },
+            })
+            return
+        }
 
-    Linking.openURL(url)
-}
+        Linking.openURL(url)
+    },
+    2500,
+    { leading: true, trailing: false }
+)
